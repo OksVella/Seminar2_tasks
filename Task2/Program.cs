329e2943ff1cf3b6768aa9cf2b4b35da1df61eed
Ovella <oks.vella@yandex.ru>
2022-10-31 04:07:01 +0300

    added task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿int num = 67903;
+if (num < 99)
+{
+    Console.Write("No third digit");
+}
+else
+{
+    if (num >= 1000)
+    {
+        int cutNumber = getCutNumber(num);
+        printThirdDigit(cutNumber);
+    }
+    else
+    {
+        printThirdDigit(num);
+    }
+}
+
+
+// --- Methods ---
+
+void printThirdDigit(int num)
+{
+    int thirdDigit = num % 10;
+    Console.Write(thirdDigit);
+}
+
+int getCutNumber(int num)
+{
+    int cutNumber = num;
+    while (cutNumber >= 1000)
+    {
+        cutNumber = cutNumber / 10;
+    }
+    return cutNumber;
+}
